5038b380ee9f7b440e30da8a4b2ecafca6569428
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-05-02 10:47:43 -0500

    Change big d helper function name

--- spec/receipt_item_spec.rb
+++ spec/receipt_item_spec.rb
@@ -2,7 +2,7 @@ require 'receipt_item'
 require 'sale_item'
 require 'bigdecimal'
 
-def D(value)
+def to_big_d(value)
   BigDecimal(value.to_s)
 end
 
@@ -11,14 +11,14 @@ RSpec.describe ReceiptItem do
     regular_item = SaleItem.new(name: "Cheap Item", price: 1)
     receipt_item = ReceiptItem.new(sale_item: regular_item, quantity: 2)
 
-    expect(receipt_item.total_price).to eq(D("2.20"))
+    expect(receipt_item.total_price).to eq(to_big_d("2.20"))
   end
 
   it "rounds correctly by rounding to the nearest 0.05" do
     regular_item = SaleItem.new(name: "Cheap Item", price: 1.01)
     receipt_item = ReceiptItem.new(sale_item: regular_item)
 
-    expect(receipt_item.send(:round_to_nearest_five_hundredths, D("1.01"))).to eq(D("1.05"))
+    expect(receipt_item.send(:round_to_nearest_five_hundredths, to_big_d("1.01"))).to eq(to_big_d("1.05"))
   end
 
   describe "taxes are set appropriately" do
@@ -26,7 +26,7 @@ RSpec.describe ReceiptItem do
       regular_item = SaleItem.new(name: "Regular Item", price: 1)
       receipt_item = ReceiptItem.new(sale_item: regular_item)
 
-      expect(receipt_item.sales_tax).to eq(D("0.10"))
+      expect(receipt_item.sales_tax).to eq(to_big_d("0.10"))
     end
 
     describe "Exempt items do not charge taxes" do
@@ -34,21 +34,21 @@ RSpec.describe ReceiptItem do
         exempt_item = SaleItem.new(name: "Chocolate", isFood: true, price: 1)
         receipt_item = ReceiptItem.new(sale_item: exempt_item)
 
-        expect(receipt_item.sales_tax).to eq(D("0.00"))
+        expect(receipt_item.sales_tax).to eq(to_big_d("0.00"))
       end
 
       it "Does not charge tax for Books" do
         exempt_item = SaleItem.new(name: "Song of Ice and Fire", isBook: true, price: 1)
         receipt_item = ReceiptItem.new(sale_item: exempt_item)
 
-        expect(receipt_item.sales_tax).to eq(D("0.00"))
+        expect(receipt_item.sales_tax).to eq(to_big_d("0.00"))
       end
 
       it "Does not charge tax for Medical items" do
         exempt_item = SaleItem.new(name: "Packet of Pills", isMedical: true, price: 1)
         receipt_item = ReceiptItem.new(sale_item: exempt_item)
 
-        expect(receipt_item.sales_tax).to eq(D("0.00"))
+        expect(receipt_item.sales_tax).to eq(to_big_d("0.00"))
       end
     end
 
@@ -56,14 +56,14 @@ RSpec.describe ReceiptItem do
       imported_item = SaleItem.new(name: "Imported Wine", imported: true, price: 1)
       receipt_item = ReceiptItem.new(sale_item: imported_item)
 
-      expect(receipt_item.sales_tax).to eq(D("0.15"))
+      expect(receipt_item.sales_tax).to eq(to_big_d("0.15"))
     end
 
     it "Rounds taxes for each individual item before multiplying" do
       multiple_items = SaleItem.new(name: "Chocolate", imported: true, isFood: true, price: 11.25)
       receipt_item = ReceiptItem.new(sale_item: multiple_items, quantity: 3)
 
-      expect(receipt_item.sales_tax).to eq(D("1.80"))
+      expect(receipt_item.sales_tax).to eq(to_big_d("1.80"))
     end
   end
 
@@ -71,6 +71,6 @@ RSpec.describe ReceiptItem do
     imported_item = SaleItem.new(name: "Imported Wine", imported: true, price: 1)
     receipt_item = ReceiptItem.new(sale_item: imported_item, quantity: 2)
 
-    expect(receipt_item.total_price).to eq(D("2.30"))
+    expect(receipt_item.total_price).to eq(to_big_d("2.30"))
   end
 end
